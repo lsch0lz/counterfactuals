6f8038be1cf5a5656df4178c2b8706c53638cb51
lsch0lz <lscholz14@googlemail.com>
2024-02-25 11:11:52 +0100

    fix: removed non-unit test

--- test/unit/test_lsat_dataloader.py
+++ test/unit/test_lsat_dataloader.py
@@ -9,19 +9,6 @@ class TestLsatDataloader(unittest.TestCase):
     DATA_DIR = str(Path(__file__).parents[2].resolve()) + "/data/"
     INPUT_DIM_VEC = [1, 1, 8, 2]
 
-    def test_if_test_and_train_file_is_downloaded(self):
-        assert os.path.isfile(self.DATA_DIR + "law_school_cf_test.csv") is False
-        assert os.path.isfile(self.DATA_DIR + "law_school_cf_test.csv") is False
-
-        data_loader = LsatDataloader(self.INPUT_DIM_VEC, self.DATA_DIR)
-        data_loader.get_lsat_dataset()
-
-        assert os.path.isfile(self.DATA_DIR + "law_school_cf_test.csv") is True
-        assert os.path.isfile(self.DATA_DIR + "law_school_cf_train.csv") is True
-
-        os.remove(self.DATA_DIR + "law_school_cf_test.csv")
-        os.remove(self.DATA_DIR + "law_school_cf_train.csv")
-
     def test_if_data_has_the_right_shape_after_trainsformation(self):
         data_loader = LsatDataloader(self.INPUT_DIM_VEC, self.DATA_DIR)
         x_train, x_test, x_means, x_stds, y_train, y_test, y_means, y_stds, DATA_KEYS, input_dims = data_loader.get_lsat_dataset()
