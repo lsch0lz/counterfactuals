17348a6d0ee13cb4f9714e03f717cde7dcdc9a53
lsch0lz <lscholz14@googlemail.com>
2024-06-10 15:33:16 +0200

    feat: added code for bnn classification

--- counterfactual_xai/methods/interpretation.py
+++ counterfactual_xai/methods/interpretation.py
@@ -4,7 +4,7 @@ import torch
 import torch.utils.data
 from matplotlib import pyplot as plt
 
-from counterfactual_xai.methods.clue import decompose_std_gauss
+from counterfactual_xai.methods.clue import decompose_std_gauss, decompose_entropy_cat
 
 
 def latent_project_gauss(BNN, VAE, dset, batch_size=1024, cuda=True, prob_BNN=True):
@@ -41,6 +41,48 @@ def latent_project_gauss(BNN, VAE, dset, batch_size=1024, cuda=True, prob_BNN=Tr
 
     tr_aleatoric_vec = torch.cat(tr_aleatoric_vec).detach().cpu().numpy()
     tr_epistemic_vec = torch.cat(tr_epistemic_vec).detach().cpu().numpy()
+    z_train = np.concatenate(z_train)
+    x_train = np.concatenate(x_train)
+    y_train = np.concatenate(y_train)
+
+    return tr_aleatoric_vec, tr_epistemic_vec, z_train, x_train, y_train
+
+
+def latent_project_cat(BNN, VAE, dset, batch_size=1024, cuda=True, prob_BNN=True):
+    if cuda:
+        loader = torch.utils.data.DataLoader(dset, batch_size=batch_size, shuffle=False, pin_memory=True,
+                                             num_workers=3)
+    else:
+        loader = torch.utils.data.DataLoader(dset, batch_size=batch_size, shuffle=False, pin_memory=False,
+                                             num_workers=3)
+    z_train = []
+    y_train = []
+    x_train = []
+    tr_aleatoric_vec = []
+    tr_epistemic_vec = []
+
+    for j, (x, y_l) in enumerate(loader):
+        zz = VAE.recongnition(x).loc.data.cpu().numpy()
+
+        # print(x.shape)
+        if prob_BNN:
+            probs = BNN.sample_predict(x, 0, False)
+            total_entropy, aleatoric_entropy, epistemic_entropy = decompose_entropy_cat(probs)
+        else:
+            probs = BNN.predict(x, grad=False)
+            total_entropy = -(probs * torch.log(probs + 1e-10)).sum(dim=1, keepdim=False)
+            aleatoric_entropy = total_entropy
+            epistemic_entropy = total_entropy * 0
+
+        tr_epistemic_vec.append(epistemic_entropy.data)
+        tr_aleatoric_vec.append(aleatoric_entropy.data)
+
+        z_train.append(zz)
+        y_train.append(y_l.numpy())
+        x_train.append(x.numpy())
+
+    tr_aleatoric_vec = torch.cat(tr_aleatoric_vec).cpu().numpy()
+    tr_epistemic_vec = torch.cat(tr_epistemic_vec).cpu().numpy()
     z_train = np.concatenate(z_train)
     x_train = np.concatenate(x_train)
     y_train = np.concatenate(y_train)
