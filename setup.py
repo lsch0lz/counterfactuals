48b674bd4e168f262a987976e42ea28a775bebfd
lsch0lz <lscholz14@googlemail.com>
2024-06-17 19:36:35 +0200

    chore: install sci-ki learn

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=find_packages(),
-    install_requires=["numpy", "tqdm", "torch", "requests", "pandas", "torchvision", "matplotlib", "pandas"],
+    install_requires=["numpy", "tqdm", "torch", "requests", "pandas", "torchvision", "matplotlib", "scikit-learn"],
 
     keywords=['python'],
     classifiers=[
