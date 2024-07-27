f2dce84c3c8c7e34194363076555edad65eac158
lsch0lz <lscholz14@googlemail.com>
2024-07-27 18:41:16 +0200

    feat: finalize LoS prediction

--- examples/evaluate_bnn_regression.py
+++ examples/evaluate_bnn_regression.py
@@ -4,7 +4,8 @@ import numpy as np
 import torch
 import pandas as pd
 
-from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.model_selection import train_test_split
 
 from counterfactual_xai.utils.clue.bnn.gaussian_bnn import GaussianBNN
 from counterfactual_xai.utils.clue.gaussian_mlp import GaussianMLP
@@ -13,17 +14,28 @@ from counterfactual_xai.utils.mimic_dataloader import MimiDataLoader
 
 torch.manual_seed(1)
 
-CSV_PATH = "./../data/"
-SAVE_DIR = "/Users/lukasscholz/repositorys/studienprojekt/counterfactuals/data/mimic/regression/length_of_stay_regression/adjusted_mean_as_return_type/state_dicts.pkl"
-CUDA = False
-# MORT_ICU
-# INPUT_DIMS = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 41]
-# LoS Regression
-INPUT_DIMS = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 41]
+SAVE_DIR = "/Users/lukasscholz/repositorys/studienprojekt/counterfactuals/data/mimic/regression/length_of_stay_regression/cleaned_dataset/state_dicts.pkl"
 LEARNING_RATE = 1e-2
 
-x_train, x_test, x_means_adjusted, x_stds_adjusted, y_train, y_test, y_means, y_stds, DATA_KEYS, input_dims = MimiDataLoader(INPUT_DIMS,
-                                                                                                                             CSV_PATH).get_mimic_dataset()
+
+df_clean = pd.read_csv("/Users/lukasscholz/repositorys/studienprojekt/counterfactuals/data/mimic/los_prediction.csv")
+
+LOS = df_clean['LOS'].values
+# Prediction Features
+features = df_clean.drop(columns=['LOS'])
+
+x_train, x_test, y_train, y_test = train_test_split(features,
+                                                    LOS,
+                                                    test_size=.20,
+                                                    random_state=0)
+
+y_stds = y_train.std()
+y_means = y_train.mean()
+x_train = x_train.to_numpy()
+x_test = x_test.to_numpy()
+
+x_train = x_train.astype(np.float32)
+x_test = x_test.astype(np.float32)
 
 trainset = DataFeed(x_train, y_train, transform=None)
 valset = DataFeed(x_test, y_test, transform=None)
@@ -31,44 +43,30 @@ valset = DataFeed(x_test, y_test, transform=None)
 input_dim = x_train.shape[1]
 width = 200
 depth = 2
-output_dim = y_train.shape[1]
+output_dim = 1
 
 mlp_gauss = GaussianMLP(input_dim, width, depth, output_dim, flatten_image=False)
 N_train = x_train.shape[0]
 
-bnn_gauss = GaussianBNN(mlp_gauss, N_train, lr=LEARNING_RATE, cuda=CUDA)
+bnn_gauss = GaussianBNN(mlp_gauss, N_train, lr=LEARNING_RATE, cuda=False)
 bnn_gauss.load_weights(SAVE_DIR)
 
-df_test = pd.read_csv("/Users/lukasscholz/repositorys/studienprojekt/counterfactuals/data/test_length_of_stay_prediction.csv")
+def calculate_mse_rmse(x_test, y_test):
+    y_pred = []
+    for val in x_test:
+        mu, sigma = bnn_gauss.predict(val)
+        mu_denormalized = mu.detach().numpy() * y_stds + y_means
+        y_pred.append(mu_denormalized)
 
-predicted_max_hours: List[float] = []
-real_max_hours: List[float] = []
+    y_pred = np.array([item[0][0] for item in y_pred])
+    mse = mean_squared_error(y_test, y_pred)
+    rmse = np.sqrt(mse)
 
-for idx, row in df_test.iterrows():
-    train_sample = row.to_list()
-    gt_max_hour = train_sample[1]
-    del train_sample[1]
-
-    # Normalize the input data
-    train_sample = np.array(train_sample)
-    x_input = (train_sample - x_means_adjusted) / x_stds_adjusted
-    x_input = x_input.astype(np.float32)
-
-    x_input = torch.tensor(x_input).unsqueeze(0)
-
-    mu, sigma = bnn_gauss.predict(x_input)
-
-    mu_denormalized = mu.detach().numpy() * y_stds + y_means
-
-    print(f"Predicted: {mu_denormalized.item()}")
-    print(f"Truth: {gt_max_hour}")
-
-    predicted_max_hours.append(mu_denormalized.item())
-    real_max_hours.append(gt_max_hour)
+    return mse, rmse
 
 
-score = mean_absolute_error(real_max_hours, predicted_max_hours)
-print("The Mean Absolute Error of our Model is {}".format(round(score, 2)))
+mse, rmse = calculate_mse_rmse(x_test, y_test)
 
-score = np.sqrt(mean_absolute_error(real_max_hours, predicted_max_hours))
-print("The Mean Absolute Error of our Model is {}".format(round(score, 2)))
+print(f"MSE: {mse}")
+
+print(f"RMSE: {rmse}")
